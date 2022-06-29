70b54f20b731f18a3d1eec8809337122e32807c3
Tomer Harari <tharari21@gmail.com>
2022-06-29 10:32:43 -0400

    Added delete functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  // Adding user input to task list
+
   // Get form element
   let form = document.querySelector("#create-task-form");
   // Get task list element
@@ -11,9 +14,25 @@ document.addEventListener("DOMContentLoaded", () => {
     let input = document.getElementById("new-task-description");
     // Getting the text value from the input tag
     let inputText = input.value;
-    // Adding user task to task list
+    // Create li element
     let li = document.createElement("li");
-    li.textContent = inputText;
+    // Add task from user input to our li tag
+    li.textContent = " " + inputText + " ";
+    // Adding user task to task list
     taskList.append(li);
+
+    // Delete Functionality
+
+    // Create delete button
+    let deleteBtn = document.createElement("button");
+    // Added text content to button
+    deleteBtn.textContent = "X";
+    // Append delete button to page
+    li.append(deleteBtn);
+    // Add delete button click event
+    deleteBtn.addEventListener("click", () => {
+      // delete the li
+      li.remove();
+    });
   });
 });
